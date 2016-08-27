2e93da6d1763d51e570d9d6f4739fd0bdc897cbd
Carlos Rivero Aro <carlosriveroaro7@gmail.com>
2016-08-27 16:14:13 +0200

    new files

---
+++ src/main/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/hybridgroup/gobot"
+	"github.com/hybridgroup/gobot/platforms/raspi"
+	"github.com/hybridgroup/gobot/platforms/gpio"
+	"time"
+)
+
+func main() {
+	gbot := gobot.NewGobot()
+
+	r := raspi.NewRaspiAdaptor("raspi")
+	led := gpio.NewLedDriver(r, "led", "7")
+
+	work := func() {
+		gobot.Every(1*time.Second, func() {
+			led.Toggle()
+		})
+	}
+
+	robot := gobot.NewRobot("blinkBot",
+		[]gobot.Connection{r},
+		[]gobot.Device{led},
+		work,
+	)
+
+	gbot.AddRobot(robot)
+
+	gbot.Start()
+}
